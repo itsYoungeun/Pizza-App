f7e6f8cedec52b0402ac8a5bb6c7ec9d7fc5e2a8
Peter <youngeunpc@yahoo.com>
2025-02-06 13:26:00 -0300

    Initial commit

---
+++ basics.ts
@@ -0,0 +1,7 @@
+// Obligatory Types
+let myName: string = "Bob" // Typescript has inferred what datatype this is supposed to be from JS
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
+
+// Defining Custom Types
