071596355e7c45fc58493f8ac57c43292751c806
Peter <youngeunpc@yahoo.com>
2025-02-06 15:53:26 -0300

    Learning typescript

--- basics.ts
+++ basics.ts
@@ -1,7 +1,80 @@
-// Obligatory Types
+// OBLIGATORY TYPES
 let myName: string = "Bob" // Typescript has inferred what datatype this is supposed to be from JS
-
 let numberOfWheels: number = 4
 let isStudent: boolean = false
 
-// Defining Custom Types
+// DEFINING CUSTOM TYPES
+type Food = string
+let favoriteFood: Food = "pizza"
+
+type Address = {
+    street: string,
+    city: string,
+    country: string,
+}
+
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean,
+    address?: Address,
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main St",
+        city: "Springfield",
+        country: "USA",
+    }
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "456 Elm St",
+        city: "Springfield",
+        country: "USA",
+    }
+}
+
+function displayInfo(person: Person) {
+    console.log(`${person.name} lives at ${person.address?.street}`)
+}
+
+displayInfo(person1)
+
+// TYPING ARRAYS
+let ages: number[] = [100, 101, 102]
+
+let person3: Person = {
+    name: "Bobby",
+    age: 42,
+    isStudent: true,
+}
+
+let person4: Person = {
+    name: "Janet",
+    age: 66,
+    isStudent: false,
+}
+
+let people: Array<Person> = [person1, person2]
+// let people: Person[] = [person1, person2]
+
+// LITERAL TYPES
+let thisName = "Bob"
+const thisName2 = "BoB"
+
+// UNIONS
+type User = {
+    username: string,
+    role: "guest" | "member" | "admin",
+}
+
+type UserRole = "guest" | "member" | "admin"
+let userRole: UserRole = "member"
--- index.ts
+++ index.ts
@@ -1,42 +1,59 @@
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+type Pizza = {
+    id: number,
+    name: string,
+    price: number,
+}
+
+type Order = {
+    id: number,
+    pizza: Pizza,
+    status: "ordered" | "completed",
+}
+
+const menu: Pizza[] = [
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 }, 
 ]
 
 let cashInRegister = 100
 let nextOrderId = 1
-const orderQueue = []
+const orderHistory: Order[] = []
 
-function addNewPizza(pizzaObj) {
+function addNewPizza(pizzaObj: Pizza) {
     menu.push(pizzaObj)
 }
 
-function placeOrder(pizzaName) {
+function placeOrder(pizzaName: string) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     if (!selectedPizza) {
-        return "Pizza not found"
+        console.error(`${pizzaName} was not found in the menu`)
+        return
     }
     cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered"}
-    orderQueue.push(newOrder)
+    const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered"}
+    orderHistory.push(newOrder)
     return newOrder
 }
 
 function completeOrder(orderId: number) {
-    const order = orderQueue.find(order => order.id === orderId)
+    const order = orderHistory.find(order => order.id === orderId)
+    if (!order) {
+        console.error(`${orderId} was not found in the orderHistory`)
+        return
+    }
     order.status = "completed"
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
 
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+console.log("Order history:", orderHistory)
